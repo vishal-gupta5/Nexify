4ccf160eb5b210cad561193919827962f00c1d85
Vishal Gupta <guptaaryan1103@gmail.com>
2026-02-23 20:53:11 +0530

    Fix the database connection bug

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,15 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/database");
 const app = express();
+dotenv.config({});
 const PORT = process.env.PORT || 6000;
 
 app.use(express.json());
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Database connection is established!")
-    console.log(`The app is successfully running on ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection is established!");
+      console.log(`The app is successfully running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Database can't be connected!`);
   });
-}).catch((err) => {
-    console.log(`Database can't be connected!`)
-});
--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -1,12 +1,11 @@
-const mongoose = require("express");
-
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_DB_URL);
-    } catch (err) {
-        console.log(`Error: ${err}`);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+  }
+};
 
-module.exports = connectDB; 
+module.exports = connectDB;
